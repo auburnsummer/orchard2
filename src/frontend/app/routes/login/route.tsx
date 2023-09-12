45d8a05b8cf152e88a540d11c2c2020ff27ef187
auburn <37142182+auburnsummer@users.noreply.github.com>
2023-09-12 03:54:16 +0000

    yeah this as well

--- src/frontend/app/routes/login/route.tsx
+++ src/frontend/app/routes/login/route.tsx
@@ -33,35 +33,6 @@ export const action = async ({request}: ActionArgs) => {
 
 }
 
-// export const loader = async ({request}: LoaderArgs) => {
-//     const url = new URL(request.url);
-//     const method = url.searchParams.get("method");
-//     const backTo = url.searchParams.get("go_back_to");
-//     if (!method) {
-//         throw new Error("Code not found")
-//     }
-//     const headers = new Headers();
-
-//     if (backTo) {
-//         headers.append("set-cookie", await createBackToCookie().serialize(backTo))
-//     }
-
-//     if (method === "discord") {
-//         const searchParams = new URLSearchParams({
-//             client_id: getEnv("DISCORD_LOGIN_APPLICATION_ID") || '',
-//             redirect_uri: `${getEnv("SITE_URL")}/discord_callback`,
-//             response_type: 'code',
-//             scope: 'identify'
-//         })
-//         const discordLoginUrl = `https://discord.com/api/oauth2/authorize?${searchParams.toString()}`;
-//         return redirect(discordLoginUrl, {
-//             headers
-//         });
-//     }
-
-//     throw new Error("login method not supported");
-// }
-
 export default function Login() {
     return redirect("/");
 }
