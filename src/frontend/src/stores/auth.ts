a3850be36d4c3e3d8422008d677bf97a0d1f77c6
auburnsummer <37142182+auburnsummer@users.noreply.github.com>
2023-10-16 08:09:00 +0000

    log out

--- src/frontend/src/stores/auth.ts
+++ src/frontend/src/stores/auth.ts
@@ -1,6 +1,50 @@
-import { useAtom } from 'jotai'
+import { atom, useAtom, useAtomValue, useSetAtom } from 'jotai'
 import { atomWithStorage } from 'jotai/utils'
+import ky from 'ky';
+import * as tg from "generic-type-guard";
 
 export const authTokenAtom = atomWithStorage('authToken', "");
 
 export const useAuthToken = () => useAtom(authTokenAtom);
+
+export const useLogOut = () => {
+    const setAuthToken = useSetAtom(authTokenAtom);
+    return () => {
+        setAuthToken("");
+    }
+}
+
+export type User = {
+    id: string;
+    name: string;
+    avatar_url?: string;
+    // we don't read cutoff atm
+}
+
+export const isUser : tg.TypeGuard<User> = tg.isLikeObject({
+    id: tg.isString,
+    name: tg.isString,
+    avatar_url: tg.isOptional(tg.isString)
+});
+
+
+export const loggedInUserAtom = atom(async (get) => {
+    const token = get(authTokenAtom);
+    if (token === "") {
+        return undefined;
+    }
+
+    const resp = await ky.get("user/me", {
+        prefixUrl: import.meta.env.VITE_API_URL,
+        headers: {
+            authorization: `Bearer ${token}`
+        }
+    });
+    const data = await resp.json();
+    if (!isUser(data)) {
+        throw new Error(`Response from /user/me did not match schema: ${JSON.stringify(data)}`);
+    }
+    return data;
+})
+
+export const useLoggedInUser = () => useAtomValue(loggedInUserAtom);
