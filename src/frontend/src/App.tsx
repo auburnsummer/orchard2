f05c14090aa854126f5243c2702d64f083210ce6
auburnsummer <37142182+auburnsummer@users.noreply.github.com>
2023-10-20 07:13:50 +0000

    404

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -5,6 +5,7 @@ import { Route, Switch } from 'wouter-preact';
 import { Home } from '@orchard/pages/Home';
 import { DiscordCallback } from './pages/DiscordCallback';
 import { PublisherDiscordRegister } from './pages/PublisherDiscordRegister';
+import { NotFound } from './pages/404';
 
 setBasePath('https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.9.0/cdn/');
 
@@ -15,7 +16,7 @@ export function App() {
             <Route path="/publisher/discord_register"><PublisherDiscordRegister /></Route>
             <Route path="/discord_callback"><DiscordCallback /></Route>
             <Route path="/"><Home/></Route>
-            <Route>This is rendered when nothing above has matched</Route>
+            <Route><NotFound /></Route>
         </Switch>
     );
 }
