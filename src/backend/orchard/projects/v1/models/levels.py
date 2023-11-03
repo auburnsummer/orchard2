703c001973adfcb2f88d07e107c28f80d47c488f
auburnsummer <37142182+auburnsummer@users.noreply.github.com>
2023-11-03 08:12:16 +0000

    volcano - damien rice

--- src/backend/orchard/projects/v1/models/levels.py
+++ src/backend/orchard/projects/v1/models/levels.py
@@ -5,12 +5,22 @@ All levels belong to a user and a publisher.
 """
 
 from __future__ import annotations
-from datetime import datetime
+import asyncio
+from datetime import datetime, timedelta
+from io import BytesIO
+from tempfile import TemporaryFile
 
 from typing import Optional
+import httpx
 from msgspec import field
+import msgspec
+from orchard.libs.bunny_storage.bunny_storage import BunnyStorage
 from orchard.libs.vitals.pydantic_model import VitalsLevelBase
+from orchard.projects.v1.core.auth import AssetURLScope, OrchardAuthScopes, OrchardAuthToken, make_token_now, requires_scopes
+from orchard.projects.v1.core.config import config
+from orchard.projects.v1.core.forward import forward_httpx
 from orchard.projects.v1.models.metadata import metadata
+from orchard.libs.vitals import analyze
 
 import sqlalchemy as sa
 
@@ -60,14 +70,19 @@ levels = sa.Table(
     sa.Column("song_altname", sa.String, nullable=True),
 
     # other stuff.
-    sa.Column("user", sa.String, sa.ForeignKey("users.id"), nullable=False),
+    # might not be the user who posted the level.
+    sa.Column("uploader", sa.String, sa.ForeignKey("users.id"), nullable=False),
     sa.Column("publisher", sa.String, sa.ForeignKey("publishers.id"), nullable=False),
 
     sa.Column("uploaded", sa.DateTime, nullable=False),
     sa.Column("approval", sa.Integer, nullable=False, default=0)
 )
 
-class Level(VitalsLevelBase):
+
+class Level(VitalsLevelBase, kw_only=True):
+    """
+    Corresponding class for Level table.
+    """
     image: str
     thumb: str
     icon: Optional[str] = field(default=None)
@@ -77,8 +92,76 @@ class Level(VitalsLevelBase):
 
     song_altname: Optional[str] = field(default=None)
 
-    user: str
+    uploader: str
     publisher: str
     uploaded: datetime = field(default_factory=datetime.now)
     approval: int = field(default=0)
 
+
+class PrefillResult(VitalsLevelBase, kw_only=True):
+    image: str
+    thumb: str
+    url: str
+    icon: Optional[str] = None
+    asset_token: str
+
+async def run_prefill(source_url: str):
+    c = config()
+    async with BunnyStorage(
+        api_key=c.BUNNY_STORAGE_API_KEY.get_secret_value(),
+        base_endpoint=c.BUNNY_STORAGE_HOSTNAME,
+        storage_zone_name=c.BUNNY_STORAGE_USERNAME,
+        public_cdn_base=c.BUNNY_CDN_URL
+    ) as bun:
+        with TemporaryFile(mode="w+b") as f:
+            async with httpx.AsyncClient() as client:
+                resp = await client.get(source_url)
+                resp.raise_for_status()
+                async for chunk in resp.aiter_bytes():
+                    f.write(chunk)
+            f.seek(0)
+            level = analyze(f)
+            # also we have to upload the rdzip now
+            # nb the function here also seeks back to 0 already
+            # todo: figure out what sorta errors can happen here?
+            rdzip_args = f, "levels", "rdzip"
+            image_args = BytesIO(level.image), "images", "png"
+            icon_args = (BytesIO(level.icon), "icons", "png") if level.icon else None
+            thumb_args = BytesIO(level.thumb), "thumbnails", "webp"
+            async with asyncio.TaskGroup() as tg:
+                tg.create_task(bun.upload_file_by_hash(*rdzip_args))
+                tg.create_task(bun.upload_file_by_hash(*image_args))
+                tg.create_task(bun.upload_file_by_hash(*thumb_args))
+                if icon_args:
+                    tg.create_task(bun.upload_file_by_hash(*icon_args))
+
+            thumb = bun.get_public_url(bun.get_url_by_hash(*thumb_args))
+            image = bun.get_public_url(bun.get_url_by_hash(*image_args))
+            icon = bun.get_public_url(bun.get_url_by_hash(*icon_args) if icon_args else None)
+            url = bun.get_public_url(bun.get_url_by_hash(*rdzip_args))
+            asset_token = make_token_now(
+                scopes=OrchardAuthScopes(
+                    Publisher_assets=AssetURLScope(
+                        image=image,
+                        thumb=thumb,
+                        icon=icon,
+                        url=url
+                    )
+                ),
+                exp_time=timedelta(days=1)
+            )
+
+            payload = {
+                **msgspec.structs.asdict(level),
+                "thumb": thumb,
+                "image": image,
+                "icon": icon,
+                "url": url,
+                "asset_token": asset_token
+            }
+            to_send = msgspec.convert(payload, PrefillResult)
+            return to_send
+
+
+def add_level():
+    pass
--- src/backend/orchard/projects/v1/routes/levels.py
+++ src/backend/orchard/projects/v1/routes/levels.py
@@ -1,35 +1,10 @@
-from datetime import timedelta
-from typing import IO, List, Tuple, Optional
-from orchard.libs.bunny_storage import BunnyStorage
-from orchard.libs.hash import sha1
-from orchard.libs.vitals.pydantic_model import VitalsLevelBase
-from orchard.projects.v1.core.auth import AssetURLScope, OrchardAuthScopes, OrchardAuthToken, make_token_now, requires_scopes
-from orchard.projects.v1.core.config import config
-from orchard.projects.v1.core.forward import forward_httpx
-from orchard.projects.v1.core.wrapper import msgspec_return, parse_body_as
+from orchard.projects.v1.core.auth import OrchardAuthToken, requires_scopes
+from orchard.projects.v1.core.wrapper import msgspec_return
+from orchard.projects.v1.models.levels import run_prefill
 from orchard.projects.v1.models.users import inject_user
 
-import httpx
-
-from orchard.libs.vitals import analyze
 from starlette.requests import Request
-from starlette.responses import Response
 
-from tempfile import TemporaryFile
-
-import msgspec
-
-from io import BytesIO
-import asyncio
-
-class VitalsLevelExport(VitalsLevelBase):
-    image: str
-    thumb: str
-    url: str
-    icon: Optional[str] = None
-
-class PrefillResult(VitalsLevelExport):
-    asset_token: str
 
 @msgspec_return(200)
 @inject_user
@@ -40,63 +15,12 @@ async def prefill_handler(request: Request):
 
     source_url = token.Publisher_add.url
 
-    c = config()
-    async with BunnyStorage(
-        api_key=c.BUNNY_STORAGE_API_KEY.get_secret_value(),
-        base_endpoint=c.BUNNY_STORAGE_HOSTNAME,
-        storage_zone_name=c.BUNNY_STORAGE_USERNAME,
-        public_cdn_base=c.BUNNY_CDN_URL
-    ) as bun:
-        with TemporaryFile(mode="w+b") as f:
-            async with httpx.AsyncClient() as client:
-                resp = await client.get(source_url)
-                try:
-                    resp.raise_for_status()
-                except httpx.HTTPStatusError as exc:
-                    return forward_httpx(resp)
-                async for chunk in resp.aiter_bytes():
-                    f.write(chunk)
-            f.seek(0)
-            level = analyze(f)
-            # also we have to upload the rdzip now
-            # nb the function here also seeks back to 0 already
-            # todo: figure out what sorta errors can happen here?
-            rdzip_args = f, "levels", "rdzip"
-            image_args = BytesIO(level.image), "images", "png"
-            icon_args = (BytesIO(level.icon), "icons", "png") if level.icon else None
-            thumb_args = BytesIO(level.thumb), "thumbnails", "webp"
-            async with asyncio.TaskGroup() as tg:
-                tg.create_task(bun.upload_file_by_hash(*rdzip_args))
-                tg.create_task(bun.upload_file_by_hash(*image_args))
-                tg.create_task(bun.upload_file_by_hash(*thumb_args))
-                if icon_args:
-                    tg.create_task(bun.upload_file_by_hash(*icon_args))
+    prefill_result = await run_prefill(source_url)
+    return prefill_result
 
-            thumb = bun.get_public_url(bun.get_url_by_hash(*thumb_args))
-            image = bun.get_public_url(bun.get_url_by_hash(*image_args))
-            icon = bun.get_public_url(bun.get_url_by_hash(*icon_args) if icon_args else None)
-            url = bun.get_public_url(bun.get_url_by_hash(*rdzip_args)),
-            asset_token = make_token_now(
-                scopes=OrchardAuthScopes(
-                    Publisher_assets=AssetURLScope(
-                        image=image,
-                        thumb=thumb,
-                        icon=icon,
-                        url=url
-                    )
-                ),
-                exp_time=timedelta(days=1)
-            )
 
-            payload = {
-                **msgspec.structs.asdict(level),
-                "thumb": thumb,
-                "image": image,
-                "icon": icon,
-                "url": url,
-                "asset_token": asset_token
-            }
-            to_send = msgspec.convert(payload, PrefillResult)
-
-    return to_send
-
+@msgspec_return(201)
+@inject_user
+@requires_scopes({"Publisher_add", "Publisher_assets"})
+async def add_level_handler(request: Request):
+    pass
