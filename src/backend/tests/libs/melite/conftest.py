bc81e8c7b82be5716537a99e6fa842baef414be4
auburnsummer <37142182+auburnsummer@users.noreply.github.com>
2023-11-07 09:55:37 +0000

    balloon - luss remix

--- src/backend/tests/libs/melite/conftest.py
+++ src/backend/tests/libs/melite/conftest.py
@@ -1,3 +1,5 @@
+from typing import Optional
+from orchard.libs.melite.base import MeliteStruct
 import pytest
 import apsw
 
@@ -6,6 +8,110 @@ import apsw.bestpractice
 
 @pytest.fixture
 def db():
+    "initial db"
     apsw.bestpractice.apply(apsw.bestpractice.recommended)
     conn = apsw.Connection(":memory:")
     yield conn
+
+
+@pytest.fixture
+def db_with_one_table(db: apsw.Connection):
+    "make bird table"
+    q = """--sql
+CREATE TABLE "bird" (
+    "id" TEXT PRIMARY KEY NOT NULL,
+    "name" TEXT NOT NULL,
+    "height_cm" INT NOT NULL,
+    "colour" TEXT NOT NULL
+) STRICT;
+    """
+    db.execute(q)
+    yield db
+
+
+@pytest.fixture
+def db_with_some_data(db_with_one_table: apsw.Connection):
+    "insert into bird table"
+    q = """--sql
+INSERT INTO "bird"
+VALUES 
+    ('aaaaa', 'wren', 5, 'blue'),
+    ('bbbbb', 'budgie', 6, 'green'),
+    ('ccccc', 'miner', 11, 'black')
+    """
+    db_with_one_table.execute(q)
+    yield db_with_one_table
+
+
+class Bird(MeliteStruct):
+    "corresponds to bird table"
+    table_name = "bird"
+    id: str
+    name: str
+    height_cm: int
+    colour: str
+
+
+@pytest.fixture
+def db_with_two_tables(db_with_some_data):
+    q = """--sql
+CREATE TABLE "watcher" (
+    "id" TEXT PRIMARY KEY NOT NULL,
+    "name" TEXT NOT NULL,
+    "fav_bird" TEXT,
+    FOREIGN KEY("fav_bird") REFERENCES "bird"("id")
+)
+    """
+    db_with_some_data.execute(q)
+    yield db_with_some_data
+
+class Watcher(MeliteStruct, kw_only=True):
+    table_name = "watcher"
+    id: str
+    name: str
+    fav_bird: Optional[Bird] = None
+
+@pytest.fixture
+def db_with_two_tables_and_data(db_with_two_tables):
+    q = """--sql
+INSERT INTO "watcher"
+VALUES
+    ('11111', 'mark', 'aaaaa'),
+    ('22222', 'anna', 'ccccc'),
+    ('33333', 'ines', 'aaaaa'),
+    ('44444', 'birdhater3000', NULL)
+    """
+    db_with_two_tables.execute(q)
+    yield db_with_two_tables
+
+
+@pytest.fixture
+def db_with_two_tables_not_null_fk(db_with_some_data):
+    q = """--sql
+CREATE TABLE "watcher" (
+    "id" TEXT PRIMARY KEY NOT NULL,
+    "name" TEXT NOT NULL,
+    "fav_bird" TEXT NOT NULL,
+    FOREIGN KEY("fav_bird") REFERENCES "bird"("id")
+)
+    """
+    db_with_some_data.execute(q)
+    yield db_with_some_data
+
+@pytest.fixture
+def db_with_two_tables_not_null_fk_and_data(db_with_two_tables_not_null_fk):
+    q = """--sql
+INSERT INTO "watcher"
+VALUES
+    ('11111', 'mark', 'aaaaa'),
+    ('22222', 'anna', 'ccccc'),
+    ('33333', 'ines', 'aaaaa')
+    """
+    db_with_two_tables_not_null_fk.execute(q)
+    yield db_with_two_tables_not_null_fk
+
+class Watcher2(MeliteStruct, kw_only=True):
+    table_name = "watcher"
+    id: str
+    name: str
+    fav_bird: Bird
--- src/backend/tests/libs/melite/test_factory.py
+++ src/backend/tests/libs/melite/test_factory.py
@@ -1,45 +1,9 @@
 from __future__ import annotations
-from typing import Optional
-from orchard.libs.melite.base import MeliteStruct
 
 from orchard.libs.melite.factory import make_row_trace
-import pytest
-
 
 from apsw import Connection
-
-@pytest.fixture
-def db_with_one_table(db):
-    q = """--sql
-CREATE TABLE "bird" (
-    "id" TEXT PRIMARY KEY NOT NULL,
-    "name" TEXT NOT NULL,
-    "height_cm" INT NOT NULL,
-    "colour" TEXT NOT NULL
-) STRICT;
-    """
-    db.execute(q)
-    yield db
-
-@pytest.fixture
-def db_with_some_data(db_with_one_table):
-    q = """--sql
-INSERT INTO "bird"
-VALUES 
-    ('aaaaa', 'wren', 5, 'blue'),
-    ('bbbbb', 'budgie', 6, 'green'),
-    ('ccccc', 'miner', 11, 'black')
-    """
-    db_with_one_table.execute(q)
-    yield db_with_one_table
-
-class Bird(MeliteStruct):
-    table_name = "bird"
-    id: str
-    name: str
-    height_cm: int
-    colour: str
-
+from tests.libs.melite.conftest import Bird, Watcher, Watcher2
 
 
 def test_factory_basic_operation(db_with_some_data: Connection):
@@ -71,37 +35,6 @@ def test_factory_basic_operation(db_with_some_data: Connection):
         )
     ]
 
-@pytest.fixture
-def db_with_two_tables(db_with_some_data):
-    q = """--sql
-CREATE TABLE "watcher" (
-    "id" TEXT PRIMARY KEY NOT NULL,
-    "name" TEXT NOT NULL,
-    "fav_bird" TEXT,
-    FOREIGN KEY("fav_bird") REFERENCES "bird"("id")
-)
-    """
-    db_with_some_data.execute(q)
-    yield db_with_some_data
-
-class Watcher(MeliteStruct, kw_only=True):
-    table_name = "watcher"
-    id: str
-    name: str
-    fav_bird: Optional[Bird] = None
-
-@pytest.fixture
-def db_with_two_tables_and_data(db_with_two_tables):
-    q = """--sql
-INSERT INTO "watcher"
-VALUES
-    ('11111', 'mark', 'aaaaa'),
-    ('22222', 'anna', 'ccccc'),
-    ('33333', 'ines', 'aaaaa'),
-    ('44444', 'birdhater3000', NULL)
-    """
-    db_with_two_tables.execute(q)
-    yield db_with_two_tables
 
 def test_factory_fk(db_with_two_tables_and_data):
     cursor = db_with_two_tables_and_data.cursor()
@@ -136,36 +69,6 @@ def test_factory_fk(db_with_two_tables_and_data):
         Watcher(id='44444', name='birdhater3000', fav_bird=None)
     ]
 
-@pytest.fixture
-def db_with_two_tables_not_null_fk(db_with_some_data):
-    q = """--sql
-CREATE TABLE "watcher" (
-    "id" TEXT PRIMARY KEY NOT NULL,
-    "name" TEXT NOT NULL,
-    "fav_bird" TEXT NOT NULL,
-    FOREIGN KEY("fav_bird") REFERENCES "bird"("id")
-)
-    """
-    db_with_some_data.execute(q)
-    yield db_with_some_data
-
-@pytest.fixture
-def db_with_two_tables_not_null_fk_and_data(db_with_two_tables_not_null_fk):
-    q = """--sql
-INSERT INTO "watcher"
-VALUES
-    ('11111', 'mark', 'aaaaa'),
-    ('22222', 'anna', 'ccccc'),
-    ('33333', 'ines', 'aaaaa')
-    """
-    db_with_two_tables_not_null_fk.execute(q)
-    yield db_with_two_tables_not_null_fk
-
-class Watcher2(MeliteStruct, kw_only=True):
-    table_name = "watcher"
-    id: str
-    name: str
-    fav_bird: Bird
 
 def test_factory_fk_not_null(db_with_two_tables_not_null_fk_and_data):
     cursor = db_with_two_tables_not_null_fk_and_data.cursor()
