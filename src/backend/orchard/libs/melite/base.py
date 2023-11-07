193c30254e4c9681a5c108b6f506d07faa06306d
auburnsummer <37142182+auburnsummer@users.noreply.github.com>
2023-11-07 10:48:29 +0000

    metric - doomscroller

--- src/backend/orchard/libs/melite/base.py
+++ src/backend/orchard/libs/melite/base.py
@@ -2,7 +2,13 @@ from __future__ import annotations
 
 from typing import ClassVar
 import msgspec
+import enum
 
+class _JSONMark(enum.Enum):
+    JSON = "JSONMARK"
+
+# Mark a field with Annotated[JSON, type] to have it be deserialized and serialized automatically."
+JSON = _JSONMark.JSON
 
 class MeliteStruct(msgspec.Struct):
     """
