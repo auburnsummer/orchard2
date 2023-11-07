effad4467c340063adbefdfa47beeb8e76eca955
auburnsummer <37142182+auburnsummer@users.noreply.github.com>
2023-11-07 09:55:37 +0000

    balloon - luss remix

---
+++ src/backend/orchard/libs/melite/select.py
@@ -0,0 +1,27 @@
+from typing import Optional, TypeVar, Type, Generic
+
+from orchard.libs.melite.factory import make_row_trace
+from .base import MeliteStruct
+
+from apsw import Connection
+
+T_co = TypeVar('T_co', bound=MeliteStruct, covariant=True)
+
+class Select(Generic[T_co]):
+    spec: Type[T_co]
+    conn: Connection
+
+    def __init__(self, conn: Connection, spec: Type[T_co]):
+        self.spec = spec
+        self.conn = conn
+
+    def by_id(self, id_: int | str) -> Optional[T_co]:
+        "Run a select query for an id and return the first result, or None if it was not found."
+        cursor = self.conn.cursor()
+        cursor.row_trace = make_row_trace(self.spec, self.conn)
+        cursor.execute(f"""--sql
+            SELECT * FROM "{self.spec.table_name}"
+            WHERE "{self.spec.table_name}"."{self.spec.primary_key}" = ?
+        """, [id_])
+
+        return next(cursor, None)
