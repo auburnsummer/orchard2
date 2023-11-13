675883ab90af6efb6d8c3b6a8f6d384654fad954
auburnsummer <37142182+auburnsummer@users.noreply.github.com>
2023-11-13 08:38:20 +0000

    discord credentials

--- src/backend/orchard/projects/v1/migrations/all.py
+++ src/backend/orchard/projects/v1/migrations/all.py
@@ -1,6 +1,8 @@
 from orchard.projects.v1.migrations.aaaaa_add_user_table import AddUserTable
+from orchard.projects.v1.migrations.bbbbb_add_discord_credentials_table import AddDiscordCredentialsTable
 
 
 ALL_MIGRATIONS = [
-    AddUserTable()
+    AddUserTable(),
+    AddDiscordCredentialsTable()
 ]
