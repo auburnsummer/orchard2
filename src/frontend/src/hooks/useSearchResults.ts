dcec90daeabd133850d6d79ed6653e350b0caaf7
auburnsummer <37142182+auburnsummer@users.noreply.github.com>
2023-12-12 11:03:22 +0000

    auto start the first search

--- src/frontend/src/hooks/useSearchResults.ts
+++ src/frontend/src/hooks/useSearchResults.ts
@@ -1,11 +1,21 @@
 import { search } from "@orchard/api/levels/levels";
 import { rdSearchParamsAtom } from "@orchard/stores/rd_search";
 import { useAsyncAction } from "./useAsync";
+import { useEffect } from "preact/hooks";
+import { tuple } from "@orchard/utils/grabbag";
 
 export function useSearchResults() {
-    return useAsyncAction(async (get, _set) => {
+    const [searchResults, startSearch] = useAsyncAction(async (get, _set) => {
         const query = get(rdSearchParamsAtom);
         const results = await search(query);
         return results;
     });
+
+    useEffect(() => {
+        if (searchResults.state === "not started") {
+            startSearch();
+        }
+    }, [searchResults]);
+
+    return tuple(searchResults, startSearch);
 }
