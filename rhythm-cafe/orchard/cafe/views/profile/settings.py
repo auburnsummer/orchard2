0e05fa9e561b6755f365b13b9cc1be4a6d2505ed
aub <37142182+auburnsummer@users.noreply.github.com>
2024-05-29 17:44:32 +1000

    settings. just theme for now. the old display settings in rcv1 will go in here as well later

--- rhythm-cafe/orchard/cafe/views/profile/settings.py
+++ rhythm-cafe/orchard/cafe/views/profile/settings.py
@@ -2,20 +2,22 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.forms import ModelForm
-from cafe.models import User
+from cafe.models import UserProfile
 
-
+class ProfileUserProfileForm(ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ["theme_pref"]
 
 @login_required
 def settings(request):
-    # if request.method == 'POST':
-    #     form = ProfileInfoForm(request.POST)
-    #     # check whether it's valid:
-    #     if form.is_valid():
-    #         request.user.first_name = form.cleaned_data.get("first_name")
-    #         request.user.save()
-    #         return HttpResponseRedirect("/accounts/profile")
-    # else:
-    #     form = ProfileInfoForm(initial={'first_name': request.user.first_name})
+    if request.method == 'POST':
+        form = ProfileUserProfileForm(request.POST)
+        print(form)
+        if form.is_valid():
+            user_prof = request.user.profile
+            user_prof.theme_pref = form.cleaned_data.get("theme_pref")
+            user_prof.save()
+            return HttpResponseRedirect("/accounts/profile/settings")
 
     return render(request, "cafe/profile/settings.jinja")
