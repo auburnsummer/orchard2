b6185dfdeda63294bb5dc681c3ac3d550b38e9c7
aub <37142182+auburnsummer@users.noreply.github.com>
2024-06-02 17:56:19 +1000

    kitten

--- rhythm-cafe/orchard/cafe/admin.py
+++ rhythm-cafe/orchard/cafe/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User, RDLevel
+from .models import User, RDLevel, Club, UserProfile
 
 # Register your models here.
 
 admin.site.register(User, UserAdmin)
 
 admin.site.register(RDLevel)
+admin.site.register(Club)
+admin.site.register(UserProfile)
