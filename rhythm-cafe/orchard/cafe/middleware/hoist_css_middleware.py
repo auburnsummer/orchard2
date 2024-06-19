c4171a55958fd624ee6f33ddeabe9d4555cfcb34
aub <37142182+auburnsummer@users.noreply.github.com>
2024-06-19 23:25:03 +1000

    css file in anticipation of swup

--- rhythm-cafe/orchard/cafe/middleware/hoist_css_middleware.py
+++ rhythm-cafe/orchard/cafe/middleware/hoist_css_middleware.py
@@ -20,19 +20,10 @@ def hoist_css_middleware(get_response):
             # all css that should be hoisted contains data-hoist=true attribute.
             style_regex = re.compile(r"<style data-hoist=\"true\">([\w\W]+?)<\/style>")
 
-            matches = []
             def substitute(match):
-                matches.append(match)
                 return ''
             
             response_content = style_regex.sub(substitute, response_content)
-
-            # matches now contains all the regex matches, turn it into one big css string
-            css_content = r"<style>" + "\n".join(match.groups()[0] for match in matches) + r"</style>"
-
-            print(css_content)
-            # which gets placed into the head. we've put a marker in head.jinja to do the replace on.
-            response_content = response_content.replace("<!-- HOISTED_CSS -->", css_content, 1)
 
             response.content = response_content.encode(response.charset) 
 
