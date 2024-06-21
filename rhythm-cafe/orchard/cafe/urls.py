0272aefeb6a11a6a9cd16e042f0e591797a8c14d
aub <37142182+auburnsummer@users.noreply.github.com>
2024-06-21 15:15:23 +1000

    iaowjefoiajwioefjio

--- rhythm-cafe/orchard/cafe/urls.py
+++ rhythm-cafe/orchard/cafe/urls.py
@@ -14,9 +14,10 @@ urlpatterns = [
     path("groups/create/", views.clubs.create_club, name="create_club"),
 
     path("groups/<club_id>/settings/", views.club_settings.info, name="club_settings_info"),
+    path("groups/<club_id>/settings/invite/", views.index, name="club_settings_membership_invite"),
     path("groups/<club_id>/settings/members/", views.club_settings.members, name="club_settings_members"),
     path("groups/<club_id>/settings/members/<user_id>/", views.club_settings.alter_membership, name="club_settings_membership"),
-    path("groups/<club_id>/settings/members/<user_id>/delete", views.club_settings.delete_membership, name="club_settings_membership_delete"),
+    path("groups/<club_id>/settings/members/<user_id>/delete/", views.club_settings.delete_membership, name="club_settings_membership_delete"),
 
     path("meta/all_styles.css", views.meta.combined_css, name="combined_css"),
 
