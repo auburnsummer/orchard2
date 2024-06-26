90e4965dabfc6bab949800371d9e9939d0bc2e4c
aub <37142182+auburnsummer@users.noreply.github.com>
2024-06-26 19:02:46 +1000

    instant chazuke

--- rhythm-cafe/orchard/cafe/admin.py
+++ rhythm-cafe/orchard/cafe/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User, RDLevel, Club, UserProfile, ClubMembership, ClubRDLevel
+from .models import User, RDLevel, Club, UserProfile, ClubMembership, ClubRDLevel, ClubInvite
 
 # Register your models here.
 
@@ -9,5 +9,6 @@ admin.site.register(User, UserAdmin)
 admin.site.register(RDLevel)
 admin.site.register(Club)
 admin.site.register(ClubMembership)
+admin.site.register(ClubInvite)
 admin.site.register(ClubRDLevel)
 admin.site.register(UserProfile)
