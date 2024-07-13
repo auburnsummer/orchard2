b185035e25734117c1a725022c8fbddcf6f78f0e
auburn <37142182+auburnsummer@users.noreply.github.com>
2024-07-13 14:46:42 +1000

    aaaaaaaaa

--- rhythm-cafe/orchard/cafe/views/discord_bot/handlers.py
+++ rhythm-cafe/orchard/cafe/views/discord_bot/handlers.py
@@ -7,6 +7,8 @@ from django.urls import reverse
 
 from orchard.settings import DOMAIN_URL
 
+from cafe.models import DiscordGuild
+
 connectgroup_signer = TimestampSigner(salt="connectgroup")
 
 def version(_data):
@@ -41,8 +43,38 @@ def connectgroup(data):
         },
     })
 
+def viewgroup(data):
+    not_found_response = JsonResponse({
+        "type": 4,
+        "data": {
+            "content": dedent(f"""
+                No group found for this server
+            """),
+            "flags": 1 << 6
+        },
+    })
+    guild_id = data['guild']['id']
+    try:
+        dg = DiscordGuild.objects.get(id=guild_id)
+    except DiscordGuild.DoesNotExist:
+        return not_found_response
+    
+    if not dg.club:
+        return not_found_response
+    
+    return JsonResponse({
+        "type": 4,
+        "data": {
+            "content": dedent(f"""
+                {dg.club.name} (id: {dg.club.id})
+            """),
+            "flags": 1 << 6
+        }
+    })
+
 
 HANDLERS = {
     "version": version,
-    "connectgroup": connectgroup
+    "connectgroup": connectgroup,
+    "viewgroup": viewgroup
 }
--- rhythm-cafe/orchard/cafe/views/discord_bot/register_commands.py
+++ rhythm-cafe/orchard/cafe/views/discord_bot/register_commands.py
@@ -19,6 +19,12 @@ COMMANDS = [
         "type": 1,
         "description": "Connect this Discord server to a group",
         "default_member_permissions": "0"
+    },
+    {
+        "name": "viewgroup",
+        "type": 1,
+        "description": "View this server's connected group",
+        "default_member_permissions": "0"
     }
 ]
 
