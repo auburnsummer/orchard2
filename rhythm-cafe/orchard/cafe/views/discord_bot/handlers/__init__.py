add126978c7dcfb1043fc310a5950e4130e773e4
auburn <37142182+auburnsummer@users.noreply.github.com>
2024-07-14 20:45:09 +1000

    more files better

---
+++ rhythm-cafe/orchard/cafe/views/discord_bot/handlers/__init__.py
@@ -0,0 +1,11 @@
+from .version import version
+from .connectgroup import connectgroup
+from .viewgroup import viewgroup
+from .add import add
+
+HANDLERS = {
+    "version": version,
+    "connectgroup": connectgroup,
+    "viewgroup": viewgroup,
+    "Add level to Rhythm Café": add
+}
