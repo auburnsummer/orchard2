299e5e8849ae6f9ae7573ba5eff6357c240eb71b
auburn <37142182+auburnsummer@users.noreply.github.com>
2024-07-14 20:45:09 +1000

    more files better

---
+++ rhythm-cafe/orchard/cafe/views/discord_bot/handlers/add.py
@@ -0,0 +1,35 @@
+from django.core.signing import TimestampSigner
+from .utils import ephemeral_response, get_club_from_guild_id
+
+from orchard.settings import DOMAIN_URL
+from django.urls import reverse
+
+addlevel_signer = TimestampSigner(salt="addlevel")
+
+def add(data):
+    not_found_response = ephemeral_response("No group found for this server (the server owner needs to use the `/connectgroup` command)")
+
+
+    club = get_club_from_guild_id(data['guild']['id'])
+    
+    if not club:
+        return not_found_response
+    
+    target_id = data['data']['target_id']
+    attachments = [a for a in data['data']['resolved']['messages'][target_id]['attachments'] if a['filename'].endswith('.rdzip')]
+    if not attachments:
+        return ephemeral_response("The post doesn't have any attachments ending with .rdzip!")
+
+    lines = []
+    for attachment in attachments:
+        secret = addlevel_signer.sign_object({
+            "level_url": attachment['proxy_url'],
+            "club_id": club.id
+        })
+        url = DOMAIN_URL + reverse("cafe:level_add", args=[secret])
+        line = f"`{attachment['filename']}`: [click here]({url})"
+        lines.append(line)
+
+    content = "\n".join(f"- {line}" for line in lines)
+    
+    return ephemeral_response(content)
