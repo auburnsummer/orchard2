b8691d065a95ca70a1cd3432df29a16f97916f84
auburn <37142182+auburnsummer@users.noreply.github.com>
2024-07-19 23:55:01 +1000

    bo rise of the teal lotus is fun

--- rhythm-cafe/libs/rdlevel_parse/src/rdlevel_parse/__main__.py
+++ rhythm-cafe/libs/rdlevel_parse/src/rdlevel_parse/__main__.py
@@ -1,24 +1,4 @@
 import sys
-import json
-from . import parse
+from . import main
 
-
-def with_first_file_or_stdin(func):
-    def inner():
-        if len(sys.argv) == 1:
-            # no arg
-            func(sys.stdin)
-        else:
-            with open(sys.argv[1], "r", encoding="utf-8-sig") as f:
-                func(f)
-
-    return inner
-
-
-@with_first_file_or_stdin
-def main(file):
-    parsed = parse(file.read())
-    print(json.dumps(parsed))
-
-
-main()
+sys.exit(main())
