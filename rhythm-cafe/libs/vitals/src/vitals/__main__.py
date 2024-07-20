ec5e883194d9b29bd8d17b9804ac1a7f12916bc9
auburn <37142182+auburnsummer@users.noreply.github.com>
2024-07-20 14:52:55 +1000

    soybean oil

--- rhythm-cafe/libs/vitals/src/vitals/__main__.py
+++ rhythm-cafe/libs/vitals/src/vitals/__main__.py
@@ -1,21 +1,5 @@
 import sys
-import pprint
-from .vitals import main
 
-try:
-    file_name = sys.argv[1]
-    with open(file_name, "rb") as f:
-        vit = main(f)
-        with open("./image.png", "wb") as f:
-            f.write(vit.image)
+from . import main
 
-        with open("./thumb.webp", "wb") as f:
-            f.write(vit.thumb);
-
-        vit.image = ""
-        vit.thumb = ""
-        vit.icon = ""
-
-        pprint.pprint(vit)
-except IndexError:
-    print("Did you pass a file name in?")
+sys.exit(main())
