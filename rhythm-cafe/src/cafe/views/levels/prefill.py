310a8bf7be8dca8ec1735a2ba37d280201a577f1
auburn <37142182+auburnsummer@users.noreply.github.com>
2024-07-23 17:39:47 +1000

    temple of eternity 1f

--- rhythm-cafe/src/cafe/views/levels/prefill.py
+++ rhythm-cafe/src/cafe/views/levels/prefill.py
@@ -1,6 +1,22 @@
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseNotAllowed
 
+from .check import check_if_ok_to_continue
+
+from huey.contrib.djhuey import db_task
+from cafe.views.discord_bot.handlers.add import addlevel_signer
+from vitals import vitals
+
+from cafe.models import Club
+
+import httpx
+
+
+@db_task()
+def _run_prefill(level_url, club_id, user):
+    club = Club.objects.get(id=club_id)
+    
+    pass
 
 
 @login_required
@@ -8,4 +24,9 @@ def prefill(request, code):
     if request.method != "POST":
         return HttpResponseNotAllowed()
 
-    
+    check_if_ok_to_continue(code, request.user)
+
+    result = addlevel_signer.unsign_object(code)
+    level_url = result['level_url']
+    club_id = result['club_id']
+    _run_prefill(level_url, club_id, request.user)
