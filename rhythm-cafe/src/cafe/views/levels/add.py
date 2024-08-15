87c60dac4a373c9293eb529d1c4acd1385a14bdd
auburn <37142182+auburnsummer@users.noreply.github.com>
2024-08-15 23:06:10 +1000

    aaaaaaaaaa

--- rhythm-cafe/src/cafe/views/levels/add.py
+++ rhythm-cafe/src/cafe/views/levels/add.py
@@ -7,8 +7,7 @@ from django.core.exceptions import BadRequest, ObjectDoesNotExist
 from datetime import timedelta
 
 from cafe.models import Club
-from django.contrib.auth.decorators import login_required
-
+from rules.contrib.views import permission_required
 from oauthlogin.models import OAuthConnection
 from cafe.models.club import is_at_least_admin
 
@@ -16,31 +15,10 @@ from django.shortcuts import render
 
 from .check import check_if_ok_to_continue
 
-from django_minify_html.decorators import no_html_minification
 
-def ok_to_continue(discord_user_id, user, club):
-    # one of these two conditions must be true:
-    #    a. the user is linked to the discord account that posted the message.
-    #    b. the user is an admin or owner of the club.
-    try:
-        OAuthConnection.objects.get(
-            provider_key='discord',
-            provider_user_id=discord_user_id,
-            user=user
-        )
-        return True
-    except OAuthConnection.DoesNotExist:
-        # we should try b as well
-        pass
 
-    return is_at_least_admin(user, club)
-
-@no_html_minification
-@login_required
-def add(request, code):
-    # throws if we can't continue
-    check_if_ok_to_continue(code, request.user)
-    
+@permission_required('prefill.ok', fn=lambda _, code: code)
+def add(request, code):    
     render_data = {
         "code": code
     }
--- rhythm-cafe/src/cafe/views/levels/from_prefill.py
+++ rhythm-cafe/src/cafe/views/levels/from_prefill.py
@@ -3,9 +3,14 @@ from cafe.models import RDLevelPrefillResult
 
 from django.contrib.auth.decorators import login_required
 
+import json
+
 @login_required
 def from_prefill(request, prefill_id):
     prefill = get_object_or_404(RDLevelPrefillResult, id=prefill_id)
+    render_data = {
+        "result": json.loads(prefill.data)
+    }
     return render(request, "cafe/levels/from_prefill.jinja", {
-        "prefill": prefill,
+        "prefill": render_data
     })
