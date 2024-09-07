fe08e02acff48b07aeb5d76dd28a4f322cf87a5a
auburn <37142182+auburnsummer@users.noreply.github.com>
2024-09-08 00:21:10 +1000

    Gabonese tea could be addicting? (5)

--- rhythm-cafe/src/cafe/views/levels/add_four_level_form.py
+++ rhythm-cafe/src/cafe/views/levels/add_four_level_form.py
@@ -3,14 +3,13 @@ from cafe.models import RDLevelPrefillResult
 
 from django.contrib.auth.decorators import login_required
 
-import json
 
 @login_required
 def add_four_level_form(request, prefill_id):
     "Stage 4: This is the form that the user fills out, and then submits."
     prefill = get_object_or_404(RDLevelPrefillResult, id=prefill_id)
     render_data = {
-        "result": json.loads(prefill.data)
+        "result": prefill.data.decode("utf-8")
     }
     return render(request, "cafe/levels/after_prefill.jinja", {
         "prefill": render_data
