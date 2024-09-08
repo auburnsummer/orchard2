74e3359e0ae4c1082266041bb2fa6202e819d6d5
auburn <37142182+auburnsummer@users.noreply.github.com>
2024-09-08 22:01:26 +1000

    Stop and think (4)

--- rhythm-cafe/src/cafe/views/levels/add_four_level_form.py
+++ rhythm-cafe/src/cafe/views/levels/add_four_level_form.py
@@ -2,8 +2,9 @@ from django.shortcuts import get_object_or_404, render
 from cafe.models import RDLevelPrefillResult
 
 from django.contrib.auth.decorators import login_required
+from django_minify_html.decorators import no_html_minification
 
-
+@no_html_minification
 @login_required
 def add_four_level_form(request, prefill_id):
     "Stage 4: This is the form that the user fills out, and then submits."
