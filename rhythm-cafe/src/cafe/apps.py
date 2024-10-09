ee53381f78defa0629ff90c1e30f02e4c878cb9a
auburn <37142182+auburnsummer@users.noreply.github.com>
2024-10-09 13:48:13 +1000

    just store it as json lmao

--- rhythm-cafe/src/cafe/apps.py
+++ rhythm-cafe/src/cafe/apps.py
@@ -4,3 +4,9 @@ from django.apps import AppConfig
 class CafeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'cafe'
+
+    def ready(self):
+        # only using it as a side effect
+        import cafe.db_signals
+        assert cafe.db_signals
+        del cafe.db_signals
---
+++ rhythm-cafe/src/cafe/db_signals.py
@@ -0,0 +1,12 @@
+# https://djangoandy.com/2024/07/08/enabling-wal-in-sqlite-in-django/
+
+from django.db.backends.signals import connection_created
+from django.dispatch import receiver
+
+@receiver(connection_created)
+def setup_sqlite_pragmas(sender, connection, **kwargs):
+    if connection.vendor == 'sqlite':
+        cursor = connection.cursor()
+        cursor.execute('PRAGMA journal_mode=wal;')
+        cursor.execute('PRAGMA busy_timeout=5000;')
+        cursor.close()
