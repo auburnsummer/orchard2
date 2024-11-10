2243eea0594a9bc7624ef14c486be6f2e4c01fbd
auburn <37142182+auburnsummer@users.noreply.github.com>
2024-11-10 18:27:29 +1000

    edit

--- rhythm-cafe/src/cafe/views/levels/level/edit_level.py
+++ rhythm-cafe/src/cafe/views/levels/level/edit_level.py
@@ -1,4 +1,6 @@
 import json
+from django.http import HttpResponseBadRequest, JsonResponse
+from django.urls import reverse
 import rules
 
 from rules.contrib.views import permission_required, objectgetter
@@ -10,14 +12,30 @@ from django_minify_html.decorators import no_html_minification
 
 from django.core.serializers.json import DjangoJSONEncoder
 
-@permission_required('cafe.change_rdlevel', fn=objectgetter(RDLevel, 'level_id'))
-def edit_level(request, level_id):
-    level = RDLevel.objects.get(id=level_id)
+import msgspec
+from cafe.views.levels.level.common import AddLevelPayload
 
+def edit_level_post(request, level: RDLevel):
+    try:
+        body = msgspec.json.decode(request.body, type=AddLevelPayload)
+        for field in msgspec.structs.fields(AddLevelPayload):
+            setattr(level, field.name, getattr(body, field.name))
+        level.save()
+
+        payload = {
+            "id": level.id,
+            "url": reverse("cafe:level_view", args=[level.id])
+        }
+
+        return JsonResponse(payload)
+    except msgspec.ValidationError as e:
+        # return 401 error response
+        return HttpResponseBadRequest(str(e))
+
+
+def edit_level_get(request, level: RDLevel):
     dict = model_to_dict(level)
     serialized = json.dumps(dict, cls=DjangoJSONEncoder)
-
-    print(serialized)
     
     render_data = {
         "prefill": serialized,
@@ -25,3 +43,11 @@ def edit_level(request, level_id):
         "mode": "edit"
     }
     return render(request, 'cafe/levels/edit_level.jinja', render_data)
+
+@permission_required('cafe.change_rdlevel', fn=objectgetter(RDLevel, 'level_id'))
+def edit_level(request, level_id):
+    level = RDLevel.objects.get(id=level_id)
+    if request.method == 'POST':
+        return edit_level_post(request, level)
+    else:
+        return edit_level_get(request, level)
