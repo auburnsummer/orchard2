f5de4a6d76d4fdc6cc510827db899719b240f2f8
auburn <37142182+auburnsummer@users.noreply.github.com>
2025-04-04 18:33:48 +1000

    add script to sync the local djangobridge

---
+++ tools/link_django_bridge.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd "$(dirname "$0")"
+cd ../
+rm django-bridge-react-0.4.0.tgz
+cd client
+npm uninstall @django-bridge/react
+cd ../
+cd django-bridge/packages/react
+npm install
+npm run build
+npm pack --pack-destination=../../../
+cd ../../../client
+npm install ../django-bridge-react-0.4.0.tgz
