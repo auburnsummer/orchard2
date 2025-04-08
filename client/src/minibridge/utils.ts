9662df0f13ce53cb50ad4afee9f2efa3297e6597
auburn <37142182+auburnsummer@users.noreply.github.com>
2025-04-08 23:23:32 +1000

    forms can be get

--- client/src/minibridge/utils.ts
+++ client/src/minibridge/utils.ts
@@ -4,3 +4,16 @@ export function makeCanonicalURL(path: string) {
     // if path is relative, it will add window.document.baseURI to it.
     return new URL(path, window.document.baseURI);
 }
+
+export function formDataToSearchParams(formData: FormData): URLSearchParams {
+    const searchParams = new URLSearchParams();
+    for (const pair of formData.entries()) {
+        const [key, value] = pair;
+        if (typeof(value) === "string") {
+            searchParams.set(key, value);
+        } else {
+            console.error("A file was passed into a GET form. This has been ignored.");
+        }
+    }
+    return searchParams;
+}
