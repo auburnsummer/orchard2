6885e59e47590726f630bb45f53c52e2c6148346
auburn <37142182+auburnsummer@users.noreply.github.com>
2025-04-23 22:11:36 +1000

    form

--- client/src/components/LevelCard/LevelCard.tsx
+++ client/src/components/LevelCard/LevelCard.tsx
@@ -3,32 +3,32 @@ import { RDLevel } from "@cafe/types/rdLevelBase"
 import cc from "clsx";
 import styles from "./LevelCard.module.css";
 
-import { Text, UnstyledButton } from "@mantine/core";
+import { Group, Text, UnstyledButton } from "@mantine/core";
 import { useClipboard } from '@mantine/hooks';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faDownload, faHeartPulse, faPaste, faPen, faPerson } from "@fortawesome/free-solid-svg-icons";
+import { faCheck, faDownload, faExclamationTriangle, faHeartPulse, faPaste, faPen, faPerson, faX, faXmark } from "@fortawesome/free-solid-svg-icons";
 import { DifficultyDecorator } from "./DifficultyDecorator/DifficultyDecorator";
 import { ConjunctionList } from "../ConjunctionList/ConjunctionList";
 import { faDiscord } from "@fortawesome/free-brands-svg-icons";
 
-type LevelCardProps = {
+type LevelCardProps = React.HTMLAttributes<HTMLDivElement> & {
     level: RDLevel;
 }
 
-export function LevelCard({level}: LevelCardProps) {
+export function LevelCard({level, className, ...rest}: LevelCardProps) {
     const clipboard = useClipboard({ timeout: 500 });
     const bpmText = level.min_bpm === level.max_bpm
         ? `${level.min_bpm} BPM`
         : `${level.min_bpm}-${level.max_bpm} BPM`;
 
     return (
-        <article className={styles.base}>
+        <article className={cc(styles.base, className)} {...rest}>
             <div className={styles.imageBox}>
                 <img className={styles.image} src={level.thumb_url} />
                 <div className={styles.overlay}>
                     <div className={styles.description}>
                         {
-                            level.description.split('\n').map(p => <Text>{p}</Text>)
+                            level.description.split('\n').map((p) => <Text>{p}</Text>)
                         }
                     </div>
                     <div className={styles.overlayButtons}>
@@ -49,8 +49,20 @@ export function LevelCard({level}: LevelCardProps) {
             <div className={styles.info}>
                 <DifficultyDecorator difficulty={level.difficulty} className={styles.decorator} />
                 <div className={styles.cast}>
-                    <h2 className={styles.song}>{level.song}</h2>
-                    <h3 className={styles.artist}>{level.artist}</h3>
+                    <div className={styles.songBox}>
+                        <h2 className={styles.song}>{level.song}</h2>
+                        {
+                            level.song_alt && (
+                                <p className={styles.songAlt}>({level.song_alt})</p>
+                            )
+                        }
+                    </div>
+                    <ConjunctionList
+                        className={styles.artists}
+                        items={level.artist_tokens}
+                        elementRender={v => <p className={styles.artist}>{v}</p>}
+                        literalRender={v => <p className={styles.artist}>{v}</p>}
+                    />
                 </div>
                 <div className={styles.metadata}>
                     <div className={cc(styles.metaitem, styles.authors)}>
@@ -75,9 +87,53 @@ export function LevelCard({level}: LevelCardProps) {
                     </div>
                     <div className={cc(styles.metaitem, styles.source)}>
                         <FontAwesomeIcon icon={faDiscord} />
-                        <Text className={cc(styles.metaitemText, styles.sourceText)}>Rhythm Doctor Lounge</Text>
+                        <Text className={cc(styles.metaitemText, styles.sourceText)}>{level.club.name}</Text>
+                    </div>
+                    <div className={
+                        cc(
+                            styles.metaitem,
+                            styles.approval,
+                            level.approval >= 10 && styles.approvalYay,
+                            level.approval < 0 && styles.approvalNope
+                        )
+                    }>
+                        {
+                            level.approval >= 10 ? (
+                                <span title={'Peer-Reviewed: a trusted member of the community has checked for correct BPM/offset, metadata, and cues to ensure playability.'}>
+                                    <FontAwesomeIcon icon={faCheck} className={styles.metaicon} />
+                                </span>
+                            ) : level.approval < 0 ? (
+                                <span title={'Non-Referred: a trusted member of the community has checked for correct BPM/offset, metadata, and cues to ensure playability, and has found that this level does not meet standards.'}>
+                                    <FontAwesomeIcon icon={faXmark} className={styles.metaicon} />
+                                </span>
+                            ) : null
+                        }
                     </div>
                 </div>
+                <div className={styles.spacer} />
+                <ul className={styles.tags}>
+                    {
+                        level.seizure_warning && (
+                            <li>
+                                <span className={cc(styles.tagCaution, styles.tag)}>
+                                    <FontAwesomeIcon icon={faExclamationTriangle} className={styles.metaicon} />
+                                    <span title="This level contains visuals and sustained flashing lighting that may affect those who are susceptible to photosensitive epilepsy or have other photosensitivities.">Seizure warning</span>
+                                </span>
+                            </li>
+                        )
+                    }
+                    {
+                        level.tags.map((tag) => (
+                            <li>
+                                <span
+                                    className={styles.tag}
+                                >
+                                    {tag}
+                                </span>
+                            </li>
+                        ))
+                    }
+                </ul>
             </div>
         </article>
     )
--- client/src/views/LevelAdd/LevelAddFromPrefill/PrefillReady/PrefillReady.tsx
+++ client/src/views/LevelAdd/LevelAddFromPrefill/PrefillReady/PrefillReady.tsx
@@ -1,30 +1,238 @@
 import { LevelCard } from "@cafe/components/LevelCard/LevelCard"
 import { Shell } from "@cafe/components/Shell"
-import { RDLevelPrefillReady } from "@cafe/types/rdLevelPrefill"
+import { RDLevelPrefill, RDLevelPrefillReady } from "@cafe/types/rdLevelPrefill"
 import commonStyles from '@cafe/theme/commonPatterns.module.css';
 import styles from "./PrefillReady.module.css";
-import { Box, Center } from "@mantine/core";
+import { Alert, TagsInput, Center, TextInput, Text, Stack, Group, Button, Textarea, NumberInput, Switch, Fieldset, Slider, Checkbox, SimpleGrid, Grid } from "@mantine/core";
 
 import cc from "clsx";
+import { useLoggedInUser } from "@cafe/hooks/useUser";
+import { useRef, useState } from "react";
+import { RDLevel } from "@cafe/types/rdLevelBase";
+import { atom, useAtom } from "jotai";
+
+import { withImmer } from "jotai-immer";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faInfoCircle } from "@fortawesome/free-solid-svg-icons";
+
+const CHECKBOXES = [
+    ["single_player", "Single Player"],
+    ["two_player", "Two Player"],
+    ["seizure_warning", "Seizure Warning"],
+    ["has_classics", "Has classic beats"],
+    ["has_oneshots", "Has Oneshots"],
+    ["has_squareshots", "Has Squareshots"],
+    ["has_freezeshots", "Has Freezeshots"],
+    ["has_freetimes", "Has Freetimes"],
+    ["has_holds", "Has Holds"],
+    ["has_skipshots", "Skipshots"],
+    ["has_window_dance", "Has Window Dance"]
+] as const;
+
+function buildInitialLevel(prefill: RDLevelPrefillReady): RDLevel {
+    return {
+        ...prefill.data,
+        song_alt: "",
+        submitter: prefill.user,
+        club: prefill.club,
+        approval: 0
+    }
+}
 
 type PrefillReadyProps = {
     prefill: RDLevelPrefillReady
 }
 
-function PrefillPreview(props: RDLevelPrefillReady) {
+function PrefillPreview({ level }: { level: RDLevel }) {
     return (
         <Center className={cc(styles.aside, commonStyles.paperBg)}>
-            <LevelCard level={props.data} />
+            <LevelCard level={level} className={styles.preview} />
         </Center>
     )
 }
 
-export function PrefillReady({prefill}: PrefillReadyProps) {
+export function PrefillReady({ prefill }: PrefillReadyProps) {
+    const user = useLoggedInUser();
+    const prefillAtom = useRef(
+        withImmer(
+            atom(
+                buildInitialLevel(prefill)
+            )
+        )
+    );
+
+    const [level, setLevel] = useAtom(prefillAtom.current);
+
+    const [bpmSync, setBpmSync] = useState(false);
+
     return (
         <Shell
-            aside={<PrefillPreview {...prefill} />}
+            aside={<PrefillPreview level={level} />}
         >
-            <p>hello!</p>
+            <Stack
+                p="md"
+                gap="md"
+            >
+                <Alert icon={<FontAwesomeIcon icon={faInfoCircle} />}>
+                    <Text>We've filled out the fields below based on the rdzip file.</Text>
+                    <Text>If it all looks OK, you can simply click "Add Level" now to add the level.</Text>
+                    <Text>Otherwise, make changes, and then click "Add Level".</Text>
+                    <Text>You will be able to come back and edit later.</Text>
+                </Alert>
+                <Group
+                    justify="space-between"
+                >
+                    <Button
+                        onClick={_ => setLevel(buildInitialLevel(prefill))}
+                    >
+                        Reset
+                    </Button>
+                    <Button>
+                        Add Level
+                    </Button>
+                </Group>
+                <Group
+                    align="end"
+                >
+                    <TextInput
+                        label="Song Name"
+                        onChange={e => setLevel(l => {
+                            l.song = e.target.value;
+                        })}
+                        value={level.song}
+                    >
+                    </TextInput>
+                    <TextInput
+                        label="Song Name (alternate)"
+                        value={level.song_alt}
+                        onChange={e => setLevel(l => {
+                            l.song_alt = e.target.value;
+                        })}
+                        description="Alternate name of the song, such as a localised or romanized name."
+                    />
+                </Group>
+                <Textarea
+                    label="Description"
+                    description="Note: <color> tags are not supported. Any <color> tags have been removed."
+                    value={level.description}
+                    onChange={e => setLevel(l => {
+                        l.description = e.target.value;
+                    })}
+                />
+                <TagsInput
+                    label="Artists"
+                    description="Press [Enter] after typing to add an artist."
+                    value={level.artist_tokens}
+                    splitChars={[]}
+                    onChange={values => setLevel(l => {
+                        if (values.length > 0) {
+                            l.artist_tokens = values;
+                        }
+                    })}
+                />
+                <Fieldset
+                    legend="BPM"
+                >
+                    <Group
+                        align="end"
+                    >
+                        <NumberInput
+                            label="Min BPM"
+                            value={level.min_bpm}
+                            min={0}
+                            max={bpmSync ? 1000 : level.max_bpm}
+                            onChange={value => setLevel(l => {
+                                if (typeof value === "number") {
+                                    l.min_bpm = value;
+                                    if (bpmSync) {
+                                        l.max_bpm = value;
+                                    }
+                                }
+                            })}
+                        />
+                        <NumberInput
+                            label="Max BPM"
+                            value={level.max_bpm}
+                            min={bpmSync ? 0 : level.min_bpm}
+                            max={1000}
+                            onChange={value => setLevel(l => {
+                                if (typeof value === "number") {
+                                    l.max_bpm = value;
+                                    if (bpmSync) {
+                                        l.min_bpm = value;
+                                    }
+                                }
+                            })}
+                        />
+                        <Switch
+                            checked={bpmSync}
+                            onChange={(event) => setBpmSync(event.currentTarget.checked)}
+                            label="Sync BPM inputs"
+                        >
+
+                        </Switch>
+                    </Group>
+                </Fieldset>
+                <Fieldset legend="Difficulty" pb="xl">
+                    <Slider
+                        label={null}
+                        step={0.001}
+                        w={300}
+                        min={0}
+                        max={3}
+                        value={level.difficulty}
+                        onChange={value => setLevel(l => {
+                            l.difficulty = value;
+                        })}
+                        restrictToMarks={true}
+                        marks={[
+                            { value: 0, label: 'Easy' },
+                            { value: 1, label: 'Medium' },
+                            { value: 2, label: 'Tough' },
+                            { value: 3, label: 'Very Tough' }
+                        ]}
+                    />
+                </Fieldset>
+                <TagsInput
+                    label="Tags"
+                    description="Press [Enter] after typing to add an tag."
+                    value={level.tags}
+                    splitChars={[]}
+                    onChange={values => setLevel(l => {
+                        if (values.length > 0) {
+                            l.tags = values;
+                        }
+                    })}
+                />
+                <Fieldset
+                    p="md"
+                >
+                    <Grid
+                        justify="flex-start"
+                        align="flex-start"
+                    >
+                        {
+                            CHECKBOXES.map(([key, label]) => {
+                                return (
+                                    <Grid.Col
+                                        span={4}
+                                    >
+                                        <Checkbox
+                                            checked={level[key]}
+                                            label={label}
+                                            onChange={(e) => setLevel(l => {
+                                                l[key] = e.target.checked;
+                                            })}
+                                        />
+                                    </Grid.Col>
+                                )
+                            })
+                        }
+                    </Grid>
+                </Fieldset>
+
+
+            </Stack>
         </Shell>
     )
 }
