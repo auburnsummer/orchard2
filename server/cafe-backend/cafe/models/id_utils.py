e97173c7fa74c72a833c270c9339c2dbf81b08e4
auburn <37142182+auburnsummer@users.noreply.github.com>
2025-05-11 09:53:24 +1000

    Parrot is even happier (3)

--- server/cafe-backend/cafe/models/id_utils.py
+++ server/cafe-backend/cafe/models/id_utils.py
@@ -4,25 +4,25 @@ from nanoid import generate
 ALPHABET = "123456789BCDFGHJKLMNPQRSTVWXYZbcdfghjklmnpqrstvwxyz"
 
 USER_ID_PREFIX = "u"
-USER_ID_LENGTH = 9
+USER_ID_LENGTH = 8
 
 def generate_user_id():
     return USER_ID_PREFIX + generate(ALPHABET, USER_ID_LENGTH - len(USER_ID_PREFIX))
 
 CLUB_ID_PREFIX = "c"
-CLUB_ID_LENGTH = 7
+CLUB_ID_LENGTH = 8
 
 def generate_club_id():
     return CLUB_ID_PREFIX + generate(ALPHABET, CLUB_ID_LENGTH - len(CLUB_ID_PREFIX))
 
 RDLEVEL_PREFILL_ID_PREFIX = "p"
-RDLEVEL_PREFILL_ID_LENGTH = 7
+RDLEVEL_PREFILL_ID_LENGTH = 8
 
 def generate_rdlevel_prefill_id():
     return RDLEVEL_PREFILL_ID_PREFIX + generate(ALPHABET, RDLEVEL_PREFILL_ID_LENGTH - len(RDLEVEL_PREFILL_ID_PREFIX))
 
 RDLEVEL_ID_PREFIX = "r"
-RDLEVEL_ID_LENGTH = 7
+RDLEVEL_ID_LENGTH = 8
 
 def generate_rdlevel_id():
-    return RDLEVEL_ID_PREFIX + generate(ALPHABET, RDLEVEL_ID_LENGTH - len(RDLEVEL_ID_LENGTH))
+    return RDLEVEL_ID_PREFIX + generate(ALPHABET, RDLEVEL_ID_LENGTH - len(RDLEVEL_ID_PREFIX))
