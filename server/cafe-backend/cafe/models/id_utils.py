b16a5347814588ec22b48264edc4f5b300b23f93
auburn <37142182+auburnsummer@users.noreply.github.com>
2025-05-10 22:14:10 +1000

    (Correction of previous message:) Backwards J.K Rowling, for instance, causing worry (4)

--- server/cafe-backend/cafe/models/id_utils.py
+++ server/cafe-backend/cafe/models/id_utils.py
@@ -7,22 +7,22 @@ USER_ID_PREFIX = "u"
 USER_ID_LENGTH = 9
 
 def generate_user_id():
-    return USER_ID_PREFIX + generate(ALPHABET, USER_ID_LENGTH)
+    return USER_ID_PREFIX + generate(ALPHABET, USER_ID_LENGTH - len(USER_ID_PREFIX))
 
 CLUB_ID_PREFIX = "c"
 CLUB_ID_LENGTH = 7
 
 def generate_club_id():
-    return CLUB_ID_PREFIX + generate(ALPHABET, CLUB_ID_LENGTH)
+    return CLUB_ID_PREFIX + generate(ALPHABET, CLUB_ID_LENGTH - len(CLUB_ID_PREFIX))
 
 RDLEVEL_PREFILL_ID_PREFIX = "p"
 RDLEVEL_PREFILL_ID_LENGTH = 7
 
 def generate_rdlevel_prefill_id():
-    return RDLEVEL_PREFILL_ID_PREFIX + generate(ALPHABET, RDLEVEL_PREFILL_ID_LENGTH)
+    return RDLEVEL_PREFILL_ID_PREFIX + generate(ALPHABET, RDLEVEL_PREFILL_ID_LENGTH - len(RDLEVEL_PREFILL_ID_PREFIX))
 
 RDLEVEL_ID_PREFIX = "r"
 RDLEVEL_ID_LENGTH = 7
 
 def generate_rdlevel_id():
-    return RDLEVEL_ID_PREFIX + generate(ALPHABET, RDLEVEL_ID_LENGTH)
+    return RDLEVEL_ID_PREFIX + generate(ALPHABET, RDLEVEL_ID_LENGTH - len(RDLEVEL_ID_LENGTH))
