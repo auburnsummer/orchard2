529ba6e1f8e0ba809e669b90bf97528ea8f28dac
auburn <37142182+auburnsummer@users.noreply.github.com>
2025-05-12 23:53:02 +1000

    Stick commercial at this location (6)

--- server/cafe-backend/cafe/views/rdlevels/view_rdlevel.py
+++ server/cafe-backend/cafe/views/rdlevels/view_rdlevel.py
@@ -2,10 +2,10 @@ from django.shortcuts import get_object_or_404
 from django_bridge.response import Response
 
 from cafe.models import RDLevel
-from cafe.views.types import AuthenticatedHttpRequest
+from cafe.views.types import HttpRequest
 
 
-def view_rdlevel(request: AuthenticatedHttpRequest, level_id: str):
+def view_rdlevel(request: HttpRequest, level_id: str):
     rdlevel = get_object_or_404(RDLevel, id=level_id)
     props = {
         "rdlevel": rdlevel.to_dict(),
