94fd9da315bce98cf12f166144e2575c0af510ca
auburn <37142182+auburnsummer@users.noreply.github.com>
2025-05-15 18:44:59 +1000

    Try drug (5)

--- server/cafe-backend/cafe/urls.py
+++ server/cafe-backend/cafe/urls.py
@@ -18,6 +18,7 @@ from .views.clubs.connect_discord import connect_discord
 from .views.login import login
 
 from .views.discord_bot.entry import entry
+from .views.rdlevels.delete_rdlevel import delete_rdlevel
 from .views.rdlevels.edit_rdlevel import edit_rdlevel
 
 from .views.rdlevels.prefill.prefill_stage_one import prefill_stage_one
@@ -51,5 +52,6 @@ urlpatterns = [
     path("levels/add/<code>/", prefill_stage_one, name="level_portal"),
     path("levels/from_prefill/<prefill_id>/", prefill_stage_two, name="level_from_prefill"),
     path("levels/<level_id>", view_rdlevel, name="level_view"),
-    path("levels/<level_id>/edit/", edit_rdlevel, name="level_edit")
+    path("levels/<level_id>/edit/", edit_rdlevel, name="level_edit"),
+    path("levels/<level_id>/delete/", delete_rdlevel, name="level_delete"),
 ]
