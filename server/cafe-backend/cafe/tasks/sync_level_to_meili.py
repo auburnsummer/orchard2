fdc2bd26d2545dbafd41f6c8917347b9a000a2b0
auburn <37142182+auburnsummer@users.noreply.github.com>
2025-05-18 16:56:35 +1000

    Bouncing on it after work is a possibility (6)

--- server/cafe-backend/cafe/tasks/sync_level_to_meili.py
+++ server/cafe-backend/cafe/tasks/sync_level_to_meili.py
@@ -1,15 +1,15 @@
-from huey.contrib.djhuey import on_commit_task
+from huey.contrib.djhuey import task
 
 from cafe.management.commands.setupmeili import RDLEVEL_INDEX_NAME
-from cafe.models import RDLevel
 from orchard.settings import MEILI_API_URL, MEILI_API_KEY
 import meilisearch
 
 
 client = meilisearch.Client(MEILI_API_URL, MEILI_API_KEY)
 
-@on_commit_task()
+@task()
 def sync_level_to_meili(level_id: str):
+    from cafe.models import RDLevel
     level = RDLevel.objects.get(id=level_id)
 
     index = client.get_index(RDLEVEL_INDEX_NAME)
