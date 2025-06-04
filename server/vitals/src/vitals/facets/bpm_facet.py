a3148bbc5aaa9b3d9a5db681807cef6f3f97d352
auburn <37142182+auburnsummer@users.noreply.github.com>
2025-06-05 00:10:24 +1000

    Something evil lurking in Pride Month (5)

--- server/vitals/src/vitals/facets/bpm_facet.py
+++ server/vitals/src/vitals/facets/bpm_facet.py
@@ -13,15 +13,7 @@ def get_bpm_from_event(evt):
         return evt["beatsPerMinute"]
 
 
-def bpm_facet(obj, toml, **kwargs):
-    # is it declared in the TOML?
-    if (
-        toml is not None
-        and try_dig(["bpm", "max"], toml)
-        and try_dig(["bpm", "min"], toml)
-    ):
-        return try_dig(["bpm", "max"], toml), try_dig(["bpm", "min"], toml)
-
+def bpm_facet(obj, **kwargs):
     bpms = [get_bpm_from_event(e) for e in obj["events"] if is_bpm_event(e)]
     # then, the bpm is either in the ["bpm"] or ["beatsPerMinute"] key
     if len(bpms) == 0:
