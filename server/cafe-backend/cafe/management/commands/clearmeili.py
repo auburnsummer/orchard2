1a58293ba51674e0ab3c5551a21315f7a1087146
auburn <37142182+auburnsummer@users.noreply.github.com>
2025-07-10 21:21:32 +1000

    sgdq

--- server/cafe-backend/cafe/management/commands/clearmeili.py
+++ server/cafe-backend/cafe/management/commands/clearmeili.py
@@ -1,9 +1,10 @@
 from django.core.management import BaseCommand
 
-from cafe.management.commands.setupmeili import client, RDLEVEL_INDEX_NAME
+from cafe.management.commands.setupmeili import get_healthy_client, RDLEVEL_INDEX_NAME
 
 class Command(BaseCommand):
     help = "clear the meilisearch index"
     def handle(self, *args, **options):
+        client = get_healthy_client()
         index = client.get_index(RDLEVEL_INDEX_NAME)
         index.delete_all_documents()
