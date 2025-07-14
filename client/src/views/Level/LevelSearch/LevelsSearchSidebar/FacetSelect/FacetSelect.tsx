5409419e5c18778675e418a489e857ff5217a216
auburn <37142182+auburnsummer@users.noreply.github.com>
2025-07-14 22:37:08 +1000

    hook up the checkboxes
    we still need to make facetselect generic later but this is ok for now

--- client/src/views/Level/LevelSearch/LevelsSearchSidebar/FacetSelect/FacetSelect.tsx
+++ client/src/views/Level/LevelSearch/LevelsSearchSidebar/FacetSelect/FacetSelect.tsx
@@ -7,14 +7,35 @@ import { Facet } from "../../LevelSearch";
 
 import { useEffect, useState } from "react";
 import { djangoGet } from "@cafe/minibridge/fetch";
+import { notifications } from "@mantine/notifications";
+import { useSearchParams } from "@cafe/minibridge/hooks";
 
 type FacetSelectProps = React.HTMLAttributes<HTMLDivElement> & {
     facetName: string;
     facets: Facet[];
 }
 
+const searchFacets = async (facetName: string, filter: string) => {
+    const newUrl = new URL(window.location.href);
+    newUrl.searchParams.set("facet_query_field", facetName);
+    newUrl.searchParams.set("facet_query", filter);
+    const result = await djangoGet(newUrl.toString());
+    if (result.action === "render") {
+        console.log("Search facets result:", result.props);
+        return result.props.facets as Facet[];
+    }  else {
+        notifications.show({
+            title: "Unknown error",
+            message: `Failed to fetch facets for ${facetName}}`,
+            color: "red",
+        })
+    }
+}
+
 export function FacetSelect({ facetName, className, facets, ...rest }: FacetSelectProps) {
     const [filter, setFilter] = useState("");
+
+    const [searchParams, navigateViaSearchParams] = useSearchParams();
 
     useEffect(() => {
         console.log(`Filter changed: ${filter}`);
@@ -40,12 +61,25 @@ export function FacetSelect({ facetName, className, facets, ...rest }: FacetSele
             <Stack gap="0.125rem">
                 {
                     facets.map(({ value, count }) => (
-                        <Checkbox key={value} label={
+                        <Checkbox
+                            key={value}
+                            defaultChecked={searchParams.getAll("tags_all").includes(value)}
+                            label={
                                 <Group gap="0.25rem" align="baseline">
                                     <Text>{value}</Text>
                                     <Text size="xs" c="dimmed">({count})</Text>
                                 </Group>
                             }
+                            onChange={(event) => {
+                                const checked = event.currentTarget.checked;
+                                navigateViaSearchParams(params => {
+                                    if (checked) {
+                                        params.append("tags_all", value)
+                                    } else {
+                                        params.delete("tags_all", value);
+                                    }
+                                })
+                            }}
                         />
                     ))
                 }
