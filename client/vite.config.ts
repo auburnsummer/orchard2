f013c819adf87baaefc1767897cafb61b19a2987
auburn <37142182+auburnsummer@users.noreply.github.com>
2025-07-17 14:17:38 +1000

    um

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+// import { analyzer } from 'vite-bundle-analyzer'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -13,7 +14,10 @@ export default defineConfig({
       input: "/src/main.tsx",
     },
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    // analyzer()
+  ],
   resolve: {
     alias: {
       "@cafe": "/src",
