82e8a70f9b0fb8113447204b5d0d8ce3272ba174
auburn <37142182+auburnsummer@users.noreply.github.com>
2025-08-12 17:03:10 +1000

    Sorcerer utilising Inner Fire and Illusion (6)

--- server/cafe-backend/cafe/management/commands/updatediscordslashcommands.py
+++ server/cafe-backend/cafe/management/commands/updatediscordslashcommands.py
@@ -7,9 +7,11 @@ so this is a one-off command that we run via manage.py
 from django.core.management.base import BaseCommand, CommandError
 from cafe.views.discord_bot.register_commands import register_commands
 
+from orchard.settings import DISCORD_BOT_TOKEN
+
 class Command(BaseCommand):
     help = "Updates discord bot commands"
 
     def handle(self, *args, **options):
-        bot_token = input("Copy paste bot token here:")
+        bot_token = DISCORD_BOT_TOKEN
         register_commands(bot_token)
