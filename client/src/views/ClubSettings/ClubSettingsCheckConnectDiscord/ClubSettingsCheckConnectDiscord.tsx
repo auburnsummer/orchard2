747fe9223f78b46ca77e58f9f13273078b4c690e
auburn <37142182+auburnsummer@users.noreply.github.com>
2025-10-22 14:14:39 +1000

    oijiojiojioijio

--- client/src/views/ClubSettings/ClubSettingsCheckConnectDiscord/ClubSettingsCheckConnectDiscord.tsx
+++ client/src/views/ClubSettings/ClubSettingsCheckConnectDiscord/ClubSettingsCheckConnectDiscord.tsx
@@ -1,17 +1,16 @@
 import { Shell } from "@cafe/components/Shell";
 import { Club } from "@cafe/types/club";
 import { ClubSettingsNavbar } from "../ClubSettingsNavbar";
-import {
-  Alert,
-  Anchor,
-  Box,
-  Button,
-  Group,
-  Text,
-  TextInput,
-} from "@mantine/core";
 import { DiscordGuild } from "@cafe/types/discordGuild";
 import { Form } from "@cafe/minibridge/components/Form";
+import { Surface } from "@cafe/components/ui/Surface";
+import { Words } from "@cafe/components/ui/Words";
+import { TextInput } from "@cafe/components/ui/TextInput";
+import { Button } from "@cafe/components/ui/Button";
+import { Alert } from "@cafe/components/ui/Alert";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faExternalLink } from "@fortawesome/free-solid-svg-icons";
+import { ExternalLink } from "@cafe/components/ui/ExternalLink";
 
 type ClubSettingsCheckConnectDiscordProps = {
   club: Club;
@@ -26,41 +25,46 @@ export function ClubSettingsCheckConnectDiscord({
 }: ClubSettingsCheckConnectDiscordProps) {
   return (
     <Shell navbar={<ClubSettingsNavbar club={club} />}>
-      <Box pt="md">
-        <Text>
-          Connecting a Discord server is done from the Discord server itself!
-        </Text>
-        <Text>
-          Please refer to{" "}
-          <Anchor href="https://github.com/auburnsummer/orchard2/wiki/Instructions-for-Server-Owners">
-            Instructions for Server Owners
-          </Anchor>{" "}
-          for how to connect.
-        </Text>
-        <Text>
-          You can use this page to check if your server is connected, by
-          entering the Server ID below:
-        </Text>
-        <Form method="GET">
-          <Group align="end">
-            <TextInput
-              type="text"
-              label="Server ID"
-              name="guild_id"
-            ></TextInput>
-            <Button type="submit">Check</Button>
-          </Group>
-        </Form>
-        {did_check && (
-          <Alert mt="md" mr="md">
-            {discord_guild === null ? (
-              <Text>Your server is not connected to this group.</Text>
-            ) : (
-              <Text>Your server is connected to this group!</Text>
-            )}
-          </Alert>
-        )}
-      </Box>
+      <Surface className="m-3 p-6 flex-grow">
+        <div>
+          <Words as="p">
+            Connecting a Discord server is done from the Discord server itself!
+          </Words>
+          <Words as="p">
+            Please refer to{" "}
+            <ExternalLink
+              href="https://github.com/auburnsummer/orchard2/wiki/Instructions-for-Server-Owners"
+            >
+              Instructions for Server Owners
+            </ExternalLink>
+            for how to connect.
+          </Words>
+          <Words as="p">
+            You can use this page to check if your server is connected, by
+            entering the Server ID below:
+          </Words>
+          <Form method="GET" className="mt-4">
+            <div className="flex items-end gap-3">
+              <TextInput
+                type="text"
+                label="Server ID"
+                name="guild_id"
+                className="flex-grow max-w-64"
+              />
+              <Button type="submit" variant="primary">Check</Button>
+            </div>
+          </Form>
+          {did_check && (
+            <Alert className="mt-4" variant={discord_guild === null ? "error" : "success"}>
+              {discord_guild === null ? (
+                <Words>Your server is not connected to this group.</Words>
+              ) : (
+                <Words>Your server is connected to this group!</Words>
+              )}
+            </Alert>
+          )}
+        </div>
+      </Surface>
     </Shell>
   );
 }
