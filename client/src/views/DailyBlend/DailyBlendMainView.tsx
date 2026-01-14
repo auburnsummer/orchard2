3e54cb03552d7dd00c497a25221e18337f68f689
auburn <37142182+auburnsummer@users.noreply.github.com>
2026-01-14 19:28:38 +1000

    ok

--- client/src/views/DailyBlend/DailyBlendMainView.tsx
+++ client/src/views/DailyBlend/DailyBlendMainView.tsx
@@ -10,7 +10,20 @@ export function DailyBlendMainView() {
         >
             <Surface className="m-3 p-4">
                 <Words variant="header" className="mb-4">
-                    Daily Blend (WIP)
+                    Daily Blend
+                </Words>
+                <Words as="p">
+                    The current time in GMT is {new Date().toUTCString()}.
+                </Words>
+                {/* <Words as="p">
+                    Blends occur at <span className="font-bold">05:00 GMT.</span>
+                </Words> */}
+                <Words as="p">
+                    Select "Blend Now" from the navigation to trigger an immediate blend.
+                    Doing so disables the scheduled blend for the day.
+                </Words>
+                <Words as="p">
+                    Please ping Auburn for any issues or feature requests.
                 </Words>
             </Surface>
         </Shell>
