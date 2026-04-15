c1c7e3a3995b9ea3b2c428196a15e4e635ffd248
auburn <37142182+auburnsummer@users.noreply.github.com>
2026-04-15 14:16:14 +1000

    no email

--- server/cafe-backend/cafe/social_adapter.py
+++ server/cafe-backend/cafe/social_adapter.py
@@ -2,11 +2,14 @@ from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
 
 
 class CafeSocialAccountAdapter(DefaultSocialAccountAdapter):
+    def populate_user(self, request, sociallogin, data):
+        user = super().populate_user(request, sociallogin, data)
+        if not user.email:
+            user.email = f"{user.id}@cafe.invalid"
+        return user
+
     def save_user(self, request, sociallogin, form=None):
         user = super().save_user(request, sociallogin, form)
         user.display_name = sociallogin.account.extra_data.get('global_name')
-        possible_email = sociallogin.account.extra_data.get('email')
-        if possible_email:
-            user.email = possible_email
         user.save()
 
