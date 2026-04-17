a8812f369e1b94a42645ff14ef24d12ded14b10c
auburn <37142182+auburnsummer@users.noreply.github.com>
2026-04-17 12:07:14 +1000

    download link

--- client/src/utils/constants.ts
+++ client/src/utils/constants.ts
@@ -1,11 +1,5 @@
 export const DIFFICULTY_STRINGS = ["Easy", "Medium", "Tough", "Very Tough"];
 
-export function getRdzipDownloadUrl(level: {
-  rdzip_url: string;
-  song: string;
-  authors: string[];
-  id: string;
-}): string {
-  const filename = `${level.song} - ${level.authors.join(", ")} ${level.id}.rdzip`;
-  return `${level.rdzip_url}?filename=${encodeURIComponent(filename)}`;
+export function getLevelDownloadUrl(level: { id: string }): string {
+  return `${window.location.origin}/levels/${level.id}/download/`;
 }
