7a9883d06c597e472598e86fa48180c397ac1718
auburn <37142182+auburnsummer@users.noreply.github.com>
2026-04-26 16:04:11 +1000

    add ebooks

--- client/src/components/Shell/Logo.tsx
+++ client/src/components/Shell/Logo.tsx
@@ -4,9 +4,42 @@ type LogoProps = {
   className?: string;
 };
 
+const EBOOKS = [
+    'Klyzx: CV why are you streaming a green line for the entire day',
+    '9thCore: most levels arent gonna have the entirety of bad apple recreated with 4x4 pixel bean decos',
+    'fizzd: banana detection',
+    'Nocallia: the macarena is syncopation',
+    'giacomo: ok, that\'s good news\ngiacomo: i have no idea whats going on',
+    'fizzd: gamedev is like: we announce game, sleep for 1 year and then wake up and press the release button',
+    'fizzd: by fixed i mean "off" has been changed to "oof"',
+    'Maddy: i only know how to make a box that says fuck in javascript but ok',
+    'DNH: just use -- snap\nDNH: dont be a coward',
+    'okamii: i needed four different pitches of beep so i made 24 instead',
+    'roninnozlo: horny anthem\nSome J Name: what>\nSome J Name: no\nSome J Name: i have never charted a horny song in my life',
+    'MarioMak967: you all are about as competent as the RHS',
+    'Kin: ah, the 🅱️iolin',
+    'Fritzy: wait there are other video games',
+    'notDonte: screw kpop in this discord we stan xeno',
+    'CV35W: Then the teacher was talking about Maple Leaf Rag and said "so this is that era\'s equivalent of a slap" and I woke up again',
+    'plastermaster: chat is going so fast nobody will realize I love you guys',
+    'lugi: lucia has been trapped in the RDCube™️\nlugi: she must atone',
+    'Szprycha: dang that fall looked real nasty\nsquareshots? insomniac cues? freezeshots? the hell are you talking about? come on let\'s go play Unwell and see who survives the longest',
+    'noche: if i could pin "MORE DEATH, MORE BLOOD, MORE COWBELL" without using up 3 pin slots i would',
+    'megaminerzero: The animator falls over like 𝚎𝚊𝚜𝚎: 𝚕𝚒𝚗𝚎𝚊𝚛',
+    '3xp0: help my audio production teacher just pulled up the all8 bpm tapper',
+    'ThatOneGuy: everyone here who can count to 7 but not 8 in Chinese, raise your hand',
+    'KubeBow: dont talk to me unless your mental breakdown sessions sample the amen break',
+    'Element 27: I will say that thinking of Edega too long causes me to emit a miasma',
+    'DPS2004: WOKE RHYTHM DOCTOR BE LIKE\none PRONOUN more\nall the GENDERS\nintimate (GAY shift)\nGabe Edega\nBISEXUAL hopper'
+];
+
+export function getEbook() {
+    return EBOOKS[Math.floor(Math.random() * EBOOKS.length)];
+}
+
 export function Logo({ className }: LogoProps) {
   return (
-    <Link href="/" className={className}>
+    <Link href="/" className={className} title={getEbook()}>
       <svg
         className="mt-3 h-11 fill-violet-400 opacity-75 hover:opacity-100 dark:fill-violet-600"
         fillRule="evenodd"
