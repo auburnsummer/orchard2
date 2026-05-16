9d9136b1d6e7ddc1cb184cc41ceabc19db19d30c
auburn <37142182+auburnsummer@users.noreply.github.com>
2026-05-16 17:21:08 +1000

    Add `build:pr` command to client (#98)
    This adds a command `build:pr` which we'll use for PR environments in
    the future.

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,6 +3,8 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 // import { analyzer } from 'vite-bundle-analyzer'
 
+const isPrBuild = process.env.VITE_PR_BUILD === "true";
+
 // https://vite.dev/config/
 export default defineConfig(() => ({
   base: "/static/",
@@ -15,6 +17,11 @@ export default defineConfig(() => ({
     rollupOptions: {
       input: "/src/main.tsx",
       output: {
+        ...(isPrBuild && {
+          entryFileNames: "[name].js",
+          chunkFileNames: "[name].js",
+          assetFileNames: "[name][extname]",
+        }),
         manualChunks: function(id) {
           if (id.includes('node_modules')) {
             return 'vendor';
