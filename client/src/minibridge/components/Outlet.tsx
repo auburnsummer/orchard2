d9ac81c9a5c4742373f53cb007f4a76ed7c50267
auburn <37142182+auburnsummer@users.noreply.github.com>
2026-05-17 23:53:45 +1000

    Reset root component state whenever the URL changes (#108)
    Fixes #106

--- client/src/minibridge/components/Outlet.tsx
+++ client/src/minibridge/components/Outlet.tsx
@@ -42,7 +42,7 @@ export function Outlet() {
         />
       )}
       <Suspense fallback={null}>
-        <View {...displayRender.props} />
+        <View key={displayRender.url} {...displayRender.props} />
       </Suspense>
     </div>
   );
