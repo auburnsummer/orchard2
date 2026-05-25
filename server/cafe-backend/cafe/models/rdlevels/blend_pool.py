45a9a54a9fc1540061c090c93182790677ffbebd
auburn <37142182+auburnsummer@users.noreply.github.com>
2026-05-25 13:54:31 +1000

    model

---
+++ server/cafe-backend/cafe/models/rdlevels/blend_pool.py
@@ -0,0 +1,12 @@
+from django.db.models import CharField
+from rules.contrib.models import RulesModel
+from cafe.models.id_utils import BLENDPOOL_ID_LENGTH, generate_blend_pool_id
+
+DEFAULT_BLEND_POOL_ID = "bdefault"
+
+class BlendPool(RulesModel):
+    id = CharField(primary_key=True, max_length=BLENDPOOL_ID_LENGTH, default=generate_blend_pool_id)
+    name = CharField(null=False, blank=False)
+
+    def __str__(self):
+        return f"Blend pool: {self.name} ({self.id})"
