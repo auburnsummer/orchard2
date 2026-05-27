ad4d5d1350c9263c48b34fc389d20d1e8e0b1618
auburn <37142182+auburnsummer@users.noreply.github.com>
2026-05-27 17:58:55 +1000

    tickets!

--- server/cafe-backend/cafe/models/rdlevels/blend_random_pool.py
+++ server/cafe-backend/cafe/models/rdlevels/blend_random_pool.py
@@ -5,11 +5,13 @@ from django.db import models
 class DailyBlendRandomPool(RulesModel):
     level = models.ForeignKey('cafe.RDLevel', on_delete=models.CASCADE)
     pool = models.ForeignKey('cafe.BlendPool', on_delete=models.CASCADE)
+    tickets = models.IntegerField(default=1)
 
     def to_dict(self):
         return {
             "level": self.level.to_dict(),
-            "pool": self.pool.to_dict()
+            "pool": self.pool.to_dict(),
+            "tickets": self.tickets
         }
 
     def __str__(self):
